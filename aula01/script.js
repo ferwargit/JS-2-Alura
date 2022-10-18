bb5a805a7ed2c51ac7308f6b335a387c47d4f6ee
Fernando Warno <ferwar@hotmail.com>
2022-10-18 09:01:16 -0300

    aula04 completada

--- aula01/script.js
+++ aula01/script.js
@@ -1,3 +1,5 @@
+( () => {
+
 const btn = document.querySelector('[data-form-btn]');
 
 // Creamos una function
@@ -10,18 +12,40 @@ const createTask = (evento) => {
     task.classList.add('card');
     // Limpiar el imput
     input.value = "";
-    const content = `<div>
+    /* const content = `<div>
                         <i class="far fa-check-square icon"></i>
+                        // Mando a llamar a la funcion
+                        ${checkComplete()}
                         <span class="task">${value}</span>
                     </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`; */
+    // console.log(checkComplete());
+    const taskContent = document.createElement("div");
+    
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    // Agrego la estructura al elemento padre
+    taskContent.appendChild(titleTask);
+    /* const content = `<div>
+                        ${checkComplete()}
+                        <span class="task">${value}</span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`; */
+    const content = `
+                        
+                        
+                    
                     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
-    console.log(content);
+    // console.log(content);
 };
 
 // ya tengo seleccionado el elemento
-console.log(btn);
+// console.log(btn);
 
 // Cuando el usuario haga click, sepamos que el usuario
 // esta realizando esa accion.
@@ -48,3 +72,34 @@ console.log(btn);
 // });
 
 btn.addEventListener("click", createTask);
+
+// Creamos una nueva funcion
+const checkComplete = () => {
+    // Creo el elemento
+    const i = document.createElement("i");
+    // Le agrego las clases
+    i.classList.add("far","fa-check-square","icon");
+    // i.classList.add("fa-check-square");
+    // i.classList.add("icon");
+    i.addEventListener("click", completeTask);
+    // regreso el elemento i
+    return i
+}
+
+// Inmediately invoked function expressions IIEF
+const completeTask = (event) => {
+    // console.log(event.target);
+    const element = event.target;
+    // Agregamos la clase fas
+    // element.classList.add("fas");
+    // toggle verifica si existe o no la clase
+    element.classList.toggle("fas");
+    // Agregamos la clase del color(css)
+    // element.classList.add('completeIcon');
+    element.classList.toggle('completeIcon');
+    // Quitaos la clase
+    // element.classList.remove("far");
+    element.classList.toggle("far");
+}
+// Para que funcione la funcion la mando a llamar inmediatamente()
+})();
